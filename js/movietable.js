d5d297e4a6dab83a2c3f0cdf5e1c823dcc94b314
UsmanSharif0001 <ussh0001@kea.stud.dk>
2024-10-04 12:06:31 +0200

    udvidet insertRowInTable metode i movietable og lavet tomt html til næste landing page

--- js/movietable.js
+++ js/movietable.js
@@ -2,9 +2,9 @@ import {fetchAnyUrl, restDelete} from "./modulejson.js";
 
 console.log("er i moviestabel")
 
-const urlMovies= "http://localhost:8080/movies" //
+const urlMovies = "http://localhost:8080/movies" //
 const tblMovies = document.getElementById("tblMovies") //
-const pbCreateMoviesTable= document.getElementById("pbGetMovies") //
+const pbCreateMoviesTable = document.getElementById("pbGetMovies") //
 
 
 function insertRowInTable(movies) { //
@@ -12,16 +12,29 @@ function insertRowInTable(movies) { //
     let rowCount = tblMovies.rows.length
     let row = tblMovies.insertRow(rowCount)
     let cell = row.insertCell(cellCount++)
-
+    let img = document.createElement('img')
     row.id = movies.movieID
+    //Filmtitel
     cell.innerHTML = movies.title
+    cell = row.insertCell(cellCount++)
+    img.src = movies.hrefImage
+    img.setAttribute("width", 300)
+    img.setAttribute("height", 300)
+    img.alt = movies.title
+    cell.appendChild(img);
+    cell = row.insertCell(cellCount++)
+    const pbBuyTicket = document.createElement("input");
+    pbBuyTicket.type = "button";
+    pbBuyTicket.setAttribute("value", "Køb billet");
+    pbBuyTicket.addEventListener('click', navigateToTicket)
 
-
-
+    cell.appendChild(pbBuyTicket);
+    console.log(row)
 
 }
 
 let movies = []
+
 async function fetchMovies() {
     movies = await fetchAnyUrl(urlMovies)
     movies.forEach(insertRowInTable)
@@ -31,4 +44,8 @@ function actionGetMovies() {
     fetchMovies()
 }
 
-pbCreateMoviesTable.addEventListener('click', actionGetMovies())
+function navigateToTicket() {
+    window.location = "buyChosenMovie"
+}
+
+pbCreateMoviesTable.addEventListener('load', actionGetMovies())
