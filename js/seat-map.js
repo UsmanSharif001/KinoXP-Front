d508662c62fe6df1669973fe3a754d74ea8183c4
thea <thsk0001@stud.kea.dk>
2024-10-07 18:14:18 +0200

    dummy seats

---
+++ js/seat-map.js
@@ -0,0 +1,7 @@
+// Select seat map element
+
+const seatMapContainer = document.getElementById("seat-map");
+
+// Based on row no. and seat no. render static seat map grid inside seat-map element
+
+
