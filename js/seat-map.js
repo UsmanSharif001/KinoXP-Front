c05b928c7ed742291bb242a8db2a11a6f0993664
thea <thsk0001@stud.kea.dk>
2024-10-08 18:32:58 +0200

    fetch screening json fetch cinema json rettet lille stavefejl i ticket

--- js/seat-map.js
+++ js/seat-map.js
@@ -4,12 +4,12 @@ const seatMapContainer = document.getElementById("seat-map");
 
 // Based on row no. and seat no. render static seat map grid inside seat-map element
 
-for (let rowCount = 1; rowCount <=25 ; rowCount++) {
+for (let rowCount = 1; rowCount <= 25; rowCount++) {
     // create row element
     const rowElement = document.createElement("div");
     rowElement.classList.add("seat-map-row"); // Add a class to the element
 
-    for (let seatCount = 1; seatCount <= 16 ; seatCount++) {
+    for (let seatCount = 1; seatCount <= 16; seatCount++) {
         // create seat elements
         const seatElement = document.createElement("div");
         seatElement.classList.add("seat-map-seat");
@@ -20,3 +20,39 @@ for (let rowCount = 1; rowCount <=25 ; rowCount++) {
     // add elements to seatMapContainer
     seatMapContainer.appendChild(rowElement);
 }
+
+const urlParams = new URLSearchParams(window.location.search);
+const screeningID = urlParams.get('screeningID');
+
+// Get screening:
+const urlScreening = `http://localhost:8080/selectedscreening/screeningID=${screeningID}`;
+
+let cinemaID = null;
+
+async function fetchScreening() {
+    if (!screeningID){
+        throw Error("Url parameter 'screeningID' is missing");
+    }
+    const screeningResponse = await fetch(urlScreening);
+    const screeningJson = await screeningResponse.json();
+
+    cinemaID = screeningJson.cinema.cinemaId;
+    // Get cinema from screening
+    const cinemaResponse = await fetch("http://localhost:8080/cinema");
+    const cinemaJson = await cinemaResponse.json();
+    console.log(cinemaJson);
+
+
+}
+
+fetchScreening();
+
+
+
+
+// Hvordan gør man det?
+
+
+
+// Get tickets from cinema ID
+// Get seats from Ticket ID's
