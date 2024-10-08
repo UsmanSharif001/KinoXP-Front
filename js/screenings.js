c57211753b8f7005b7bf70a9c1ba86784f871957
NikoP93 <nipe0001@stud.kea.dk>
2024-10-08 10:25:33 +0200

    Screenings works, but its still messy

--- js/screenings.js
+++ js/screenings.js
@@ -2,46 +2,32 @@ import {fetchAnyUrl} from "./modulejson.js";
 
 console.log("Jeg er i screenings")
 
-const pbGetmovieID = document.getElementById("pbGetMovieID")
-
-pbGetmovieID.addEventListener('click', get)
-function get () {
-    // (A) GET FROM SESSION STORAGE
-    var movieTitle = sessionStorage.getItem("movieTitle")
-
-    // (B) IT WORKS!
-    console.log(movieTitle);
-}
-
-// (C) CLEAR SESSION STORAGE
-//sessionStorage.removeItem("KEY");
-//sessionStorage.clear();
-
-
-
-console.log("Jeg er i screening")
-
-const movieID = 1;
+const movieID = sessionStorage.getItem("movieID")
 const urlScreenings = `http://localhost:8080/movies/${movieID}/screenings`
 const daysDiv = document.getElementById("days")
 const sessionsDiv = document.getElementById("sessions")
 let screenings = []
 
-function insertInToSession(screening) {
-    const sessionElement = document.createElement("div")
-    sessionElement.classList.add('session')
-    sessionElement.innerHTML = "<div>Time: ${screening.timeOfDay}</div> <div>Room: ${screening.cinema.name}</div>"
+console.log(movieID)
 
-    sessionElement.appendChild(sessionElement)
 
-}
 
-function insertInToDays(screening) {
+function insertDayAndTime(screeningDate,sessions){
     const dayElement = document.createElement("div")
-    dayElement.classList.add('day')
-    dayElement.textContent = screening.date
+    dayElement.classList.add("day");
+    dayElement.textContent = screeningDate
     daysDiv.appendChild(dayElement)
+
+    sessions.forEach(session => {
+        const sessionElement = document.createElement("div")
+        sessionElement.classList.add("session")
+        sessionElement.textContent = session.timeOfDay
+        daysDiv.appendChild(sessionElement)
+    })
 }
+
+
+
 
 async function fetchScreenings() {
     try {
@@ -57,10 +43,12 @@ async function fetchScreenings() {
         const uniqueDays = [...new Set(screenings.map(screening => screening.date))];
 
         // Insert unique days into the days grid
-        uniqueDays.forEach(insertInToDays);
+        uniqueDays.forEach(day => {
+            const screeningDays = screenings.filter(screening => screening.date === day)
+            insertDayAndTime(day,screeningDays)
+        })
 
-        // Insert all screenings into the sessions grid
-        screenings.forEach(insertInToSession);
+
 
     } catch (error) {
         console.error("Error fetching screenings:", error);
