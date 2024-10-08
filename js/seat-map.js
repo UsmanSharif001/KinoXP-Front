7a76595fb319de9f03ba8c6ec7a4c80e84d38e2b
thea <thsk0001@stud.kea.dk>
2024-10-08 09:08:52 +0200

    dummy seats

--- js/seat-map.js
+++ js/seat-map.js
@@ -4,4 +4,19 @@ const seatMapContainer = document.getElementById("seat-map");
 
 // Based on row no. and seat no. render static seat map grid inside seat-map element
 
+for (let rowCount = 1; rowCount <=25 ; rowCount++) {
+    // create row element
+    const rowElement = document.createElement("div");
+    rowElement.classList.add("seat-map-row"); // Add a class to the element
 
+    for (let seatCount = 1; seatCount <= 16 ; seatCount++) {
+        // create seat elements
+        const seatElement = document.createElement("div");
+        seatElement.classList.add("seat-map-seat");
+        seatElement.innerHTML = "🟩"
+        // add seat elements to row elements
+        rowElement.appendChild(seatElement);
+    }
+    // add elements to seatMapContainer
+    seatMapContainer.appendChild(rowElement);
+}
